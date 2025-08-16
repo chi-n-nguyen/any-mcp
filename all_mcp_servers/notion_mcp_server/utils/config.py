f938d32ecf94f528cf99f525653307b34362cd20
Minh Triet Pham <minhtrietp@student.unimelb.edu.au>
2025-08-17 00:05:25 +1000

    Clean code for all_mcp_servers/notion_mcp_server/utils/config.py

--- all_mcp_servers/notion_mcp_server/utils/config.py
+++ all_mcp_servers/notion_mcp_server/utils/config.py
@@ -2,14 +2,14 @@
 Configuration management for Notion MCP Server
 """
 
-import config
-import tools_for_mcp_server.config_mcp_tools_path as mcp_tool_path
+import config as global_config
+from tools_for_mcp_server import config_mcp_tools_path as mcp_tool_path
 
 # For NOTION config
-notion_api_version = config.NOTION_API_VERSION
-notion_base_url = config.NOTION_BASE_URL
-notion_api_token = config.NOTION_API_TOKEN
-notion_api_key = config.NOTION_API_KEY
+notion_api_version = global_config.NOTION_API_VERSION
+notion_base_url = global_config.NOTION_BASE_URL
+notion_api_token = global_config.NOTION_API_TOKEN
+notion_api_key = global_config.NOTION_API_KEY
 
 # Directory path for Notion tools here
 notion_tool_dir_path = mcp_tool_path.NOTION_TOOL_DIR_PATH
